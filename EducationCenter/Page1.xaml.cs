b6ee9b5b9d3210bd40557fd60535f6e3e73ba938
uzzy97 <G00345816@gmit.ie>
2017-12-03 16:56:40 +0000

    first commit

---
+++ EducationCenter/Page1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+
+namespace EducationCenter
+{
+
+    public sealed partial class Page1 : Page
+    {
+
+        public Page1()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void btnHome_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+
+        private void button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page2), null);
+        }
+    }
+}
